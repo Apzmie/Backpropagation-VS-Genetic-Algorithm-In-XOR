a8d2f6d3f3d325fe74310eb13212fb5af8b38a47
Apzmie <aserd12@gmail.com>
2025-06-02 00:31:35 +0900

    Update run_genetic_algorithm.py

--- run_genetic_algorithm.py
+++ run_genetic_algorithm.py
@@ -5,5 +5,5 @@ X = np.array([[0,0],[0,1],[1,0],[1,1]], dtype=np.float32)
 ytrue = np.array([[0],[1],[1],[0]], dtype=np.float32)
 
 ga = XORModel_GeneticAlgorithm(num_organisms=10, num_genes=17, num_offsprings=20, X=X, ytrue=ytrue)
-best_organism, best_loss = ga.train(generations=500, print_interval=100)
+best_organism, best_loss = ga.train(generations=200, print_interval=50)
 output = ga.predict(X, best_organism)
